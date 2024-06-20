6f2c71eae1ffc4ee2e6314d7aeb8c0f1ff741113
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-06-20 17:00:30 +0100

    Implement get wallet transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {fundWallet, transferFunds, withdrawFunds} from "../controllers/transaction.controllers";
+import {fundWallet, getWalletTransactions, transferFunds, withdrawFunds} from "../controllers/transaction.controllers";
 import {authenticate} from "../helpers/middlewares/authMiddleware";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/deposit", authenticate, fundWallet);
 router.post("/withdraw", authenticate, withdrawFunds);
 router.post("/transfer", authenticate, transferFunds);
+router.get("/:walletId", authenticate, getWalletTransactions);
 
 export default router;

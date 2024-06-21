f41dcf1b5bb5cb3d120fb8138bf8e682da5f4a26
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-06-21 11:06:29 +0100

    Update index page

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Application} from "express";
+import express, {Application, Request, Response} from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -11,15 +11,16 @@ import {dbConnection} from "./db/dbConnection";
 dotenv.config();
 
 const app: Application = express();
+const apiBaseURL = process.env.API_BASE_URL || "/api/v1";
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors());
 
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/users", usersRoutes);
-app.use("/api/v1/wallets", transactionRoutes);
+app.use(`${apiBaseURL}/auth`, authRoutes);
+app.use(`${apiBaseURL}/users`, usersRoutes);
+app.use(`${apiBaseURL}/wallets`, transactionRoutes);
 
 app.use(notFoundErrorHandler);
 app.use(generalErrorHandler);
@@ -27,6 +28,12 @@ app.use(generalErrorHandler);
 dbConnection();
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+// Welcome route
+app.get(apiBaseURL, (_req: Request, res: Response) => {
+    res.status(200).send("Welcome to the Demo Credit API");
 });
